29cd645783aa3bc94f94c4ce2df1df6d9e73c1c6
Prince Kumar Chaurasia <princechaurasiaofficial24@gmail.com>
2024-03-19 01:05:35 +0530

    small change

--- src/context/message.context.js
+++ src/context/message.context.js
@@ -21,8 +21,8 @@ export const MessageContextProvider = ({ children }) => {
         });
       }
     });
-  }, []);
-  
+  });
+
   return (
     <messageContext.Provider value={{ message }}>
       {children}
